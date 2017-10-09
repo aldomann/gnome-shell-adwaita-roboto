6f6fa116ab8cd498c7f2ade4f87f643e8dee5b7c
Alfredo Hernández <aldomann.designs@gmail.com>
2017-10-09 17:22:47 +0200

    Transition to 3.26

--- theme/parse-sass.sh
+++ theme/parse-sass.sh
@@ -1,3 +1,10 @@
-#!/usr/bin/bash
+#!/bin/sh
 
-bundle exec sass --update --sourcemap=none .
+srcdir=`dirname $0`
+stamp=${1}
+for scss in $srcdir/*.scss
+do
+  sassc -a $scss ${scss%%.scss}.css || exit 1
+done
+
+[ "$stamp" ] && touch $stamp
